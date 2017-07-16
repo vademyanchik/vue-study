143412906c28a64d2aee16c8a8c1b7524926cd5f
vademyanchik <vadim.demyanchik@gmail.com>
2017-07-16 14:48:46 +0300

    Nesting Components

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <p>{{ copyrigth }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      copyrigth: '2017 '
+    }
+  },
+}
+</script>
+
+<style scoped>
+ footer {
+  background: #222;
+  padding: 6px;
+ }
+
+ p {
+  color: lightgreen;
+  text-align: center;
+ }
+</style>
